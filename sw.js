25e3405342dcb711e352f5d95d9292295aeae052
OrangeLin2312 <1471199572@qq.com>
2024-11-15 21:34:34 +0800

    Site updated: 2024-11-15 21:34:34

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1731674731024";
+const VERSION = "1731677633111";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
