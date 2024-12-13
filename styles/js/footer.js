4fbdc36bfc3e88bfc39b13e62ceb64dacd79df0a
OrangeLin2312 <1471199572@qq.com>
2024-12-13 23:03:09 +0800

    Site updated: 2024-12-13 23:03:08

--- styles/js/footer.js
+++ styles/js/footer.js
@@ -1,3 +1,7 @@
 // $(document).ready(function() {
 //     $("#footer-wrap").css("position", "block");
 // });
+function firework() {
+    // 页面跳转到指定的URL
+    window.location.href = 'https://yanhua.w3h5.com/';
+}
